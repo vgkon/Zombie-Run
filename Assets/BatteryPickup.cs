d2b6bb7db68488d75dd0edc527a124333b1f4943
vgkon <vgkonstantakopoulos@gmail.com>
2020-09-28 19:53:00 +0300

    Revert "initial commit 2"
    This reverts commit d050266dae1f64ce4248f3d4679a5a681207786b.

---
+++ Assets/BatteryPickup.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatteryPickup : MonoBehaviour
+{
+    /*[SerializeField] float restoreAngle = 30f;
+    [SerializeField] float addIntensity = 1f;*/
+    [SerializeField] float addBattery = 0.4f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            other.GetComponentInChildren<FlashLight>().RestoreBattery(addBattery);
+        }
+        Destroy(gameObject);
+    }
+
+}
